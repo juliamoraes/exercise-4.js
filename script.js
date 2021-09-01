cda5d875d662cb72a9ccf6210ab1b08a66253ade
juliamoraes <68563285+juliamoraes@users.noreply.github.com>
2021-09-01 16:27:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let name = window.prompt(`Qual seu nome?`)
+document.write(`Seu nome em letra maiscula: ${name.toUpperCase()}`)
+document.write(`Seu nome em letra minuscula: ${name.toLowerCase()}`)
+document.write(`Seu nome tem ${name.length} letras`)
